d8f93e79413ea6c06142505b6d258cada10ba560
Kulpot <kulpotwahu@gmail.com>
2024-06-27 02:01:16 +0800

    C++ new and delete operators

--- C++ new and delete operators/CppScratchPad.cpp
+++ C++ new and delete operators/CppScratchPad.cpp
@@ -2,7 +2,7 @@
 using std::cout;
 using std::endl;
 
-//ref link:https://www.youtube.com/watch?v=_749lj2yb8Y&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1
+//C++ new and delete operators   //ref link:https://www.youtube.com/watch?v=_749lj2yb8Y&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1
 
 void main()
 {
