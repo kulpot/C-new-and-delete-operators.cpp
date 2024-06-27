06d0bfdb9a545bc0c22853c5a1eab434406c474a
Kulpot <kulpotwahu@gmail.com>
2024-06-28 05:41:56 +0800

    ..

--- C++ new and delete operators/CppScratchPad.cpp
+++ C++ new and delete operators/CppScratchPad.cpp
@@ -9,5 +9,6 @@ void main()
 	//int i = 5;
 	//int i = new int(5); //error:int* i = new int(5);
 	int* i = new int(5);  // i is now a pointer that can be stored in the heap
+	cout << *i << endl;
 
 }
