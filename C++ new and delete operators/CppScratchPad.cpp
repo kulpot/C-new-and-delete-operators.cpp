8823f92338886818a457abdbabf5c4c80d893978
Kulpot <kulpotwahu@gmail.com>
2024-06-29 11:55:40 +0800

    ..

--- C++ new and delete operators/CppScratchPad.cpp
+++ C++ new and delete operators/CppScratchPad.cpp
@@ -4,15 +4,33 @@ using std::endl;
 
 //C++ new and delete operators   //ref link:https://www.youtube.com/watch?v=_749lj2yb8Y&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1
 
+class Person
+{
+
+};
+
 void main()
 {
-	//int i = 5;
-	//int i = new int(5); //error:int* i = new int(5);
-	int* i = new int(5);  // i is now a pointer that can be stored in the heap
-	cout << *i << endl;
-	*i = 8;
-	cout << *i << endl;
-	delete i;	//delete memory i if your done with that RAM
-	cout << *i << endl;	//result: undefined
 
 }
+
+
+
+
+
+
+
+
+
+//void main()
+//{
+//	//int i = 5;
+//	//int i = new int(5); //error:int* i = new int(5);
+//	int* i = new int(5);  // i is now a pointer that can be stored in the heap
+//	cout << *i << endl;
+//	*i = 8;
+//	cout << *i << endl;
+//	delete i;	//delete memory i if your done with that RAM
+//	cout << *i << endl;	//result: undefined
+//
+//}
