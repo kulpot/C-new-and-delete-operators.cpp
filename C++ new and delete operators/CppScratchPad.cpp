ab201128a66560ae3b4197936e707ba60deed3c9
Kulpot <kulpotwahu@gmail.com>
2024-07-01 12:23:11 +0800

    ....

--- C++ new and delete operators/CppScratchPad.cpp
+++ C++ new and delete operators/CppScratchPad.cpp
@@ -3,6 +3,7 @@ using std::cout;
 using std::endl;
 
 //C++ new and delete operators   //ref link:https://www.youtube.com/watch?v=_749lj2yb8Y&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1
+//detecting memory leaks/loss by doing profiling and ....
 
 class Person
 {
